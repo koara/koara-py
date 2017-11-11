887ad6df63c8fa2eb10897a2ed4e6b066fd0414e
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2017-11-11 18:03:00 +0100

    update setup file

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
-#! /usr/bin/env python
-from setuptools import setup
+#!/usr/bin/env python
 
-setup(
-    name='koara'
-)
+from distutils.core import setup
+
+setup(name='koara',
+      version='0.11.0',
+      description='Koara parser written in Python',
+      author='Andy Van Den Heuvel',
+      author_email='andy.vandenheuvel@gmail.com',
+      url='https://github.com/koara/koara-py',
+      py_modules=['koara']
+      )
